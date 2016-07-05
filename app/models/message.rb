827c67303b28fc9a5f105fead0ad4630e9479011
yokomizo <yokomizo@fiilse.com>
2016-07-05 05:32:33 +0000

    heroku_challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ActiveRecord::Base
       validates :name , length: { maximum: 20 } , presence: true
       validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+      validates :age, numericality: {
+            only_integer: true, greater_than_or_equal_to: 0
+          }
+      validates :age,  presence: true, length: { maximum: 3 }
 end
